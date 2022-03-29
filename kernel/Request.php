42b5a2c1c51988fcea395d6e7325b32253f3b63c
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-03-29 00:17:22 -0300

    feat: created methods to extract resquest input

--- kernel/Request.php
+++ kernel/Request.php
@@ -6,11 +6,17 @@ class Request
 {
     public readonly string $method;
     public readonly string $uri;
+    public readonly object $query;
+    public readonly object $formData;
+    public readonly object $json;
 
     public function build()
     {
         $this->setUri();
         $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->setQuery();
+        $this->setFormData();
+        $this->setJson();
 
         return $this;
     }
@@ -20,11 +26,28 @@ class Request
         $this->uri = $this->normalizeUri($_SERVER['REQUEST_URI']);
     }
 
-    private function normalizeUri(string $url): string
+    private function normalizeUri(string $uriFull): string
     {
-        $limit = substr($url, -1) === '/' ? -1 : null;
-        $uri = substr($url, 1, $limit);
+        $uri = explode('?', $uriFull)[0];
+
+        $limit = substr($uri, -1) === '/' ? -1 : null;
+        $uri = substr($uri, 1, $limit);
 
         return empty($uri) ? '/' : $uri;
+    }
+
+    private function setQuery()
+    {
+        $this->query = (object) $_GET;
+    }
+
+    private function setFormData()
+    {
+        $this->formData = (object) $_POST;
+    }
+
+    private function setJson()
+    {
+        $this->json = json_decode(file_get_contents('php://input'));
     }
 }
