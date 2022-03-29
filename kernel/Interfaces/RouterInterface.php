46314f60f37667c283e6410285662c3e845fccf9
André Filippe dos Santos <fellipe.mrt@gmail.com>
2022-03-28 23:51:15 -0300

    fix: updated interface

--- kernel/Interfaces/RouterInterface.php
+++ kernel/Interfaces/RouterInterface.php
@@ -2,8 +2,16 @@
 
 namespace Kernel\Interfaces;
 
+use Kernel\Request;
+use Kernel\Route;
+
 interface RouterInterface
 {
-    public function get(string $uri, string $controller, string $method): void;
-    public function getRouteInfo(string $uri): array;
+    public function request(string $method, string $uri, string $controller, string $action): void;
+    public function get(string $uri, string $controller, string $action): void;
+    public function post(string $uri, string $controller, string $action): void;
+    public function put(string $uri, string $controller, string $action): void;
+    public function patch(string $uri, string $controller, string $action): void;
+    public function delete(string $uri, string $controller, string $action): void;
+    public function getRoute(Request $request): Route;
 }
